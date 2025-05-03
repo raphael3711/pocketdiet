68fa555b438d9610685d1bb4dd9abad870ff57eb
raphael3711 <r.sayan@promodo.com>
2025-05-03 17:54:05 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,8 +1,1 @@
-
-function changeLanguage() {
-    const lang = document.getElementById('language').value;
-    alert('Выбран язык: ' + lang + ' (перевод реализуется позже)');
-}
-function scan() {
-    alert('Открытие камеры для сканирования продукта (функционал в разработке).');
-}
+// JS logic here
