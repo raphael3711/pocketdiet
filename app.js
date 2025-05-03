fcf92c25ef32d70dd5069a288c067eac4896566c
raphael3711 <r.sayan@promodo.com>
2025-05-03 07:28:31 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
-console.log("Диетолог в кармане работает!");
+
+function scanProduct() {
+    const video = document.getElementById('preview');
+    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+        navigator.mediaDevices.getUserMedia({ video: { facingMode: "environment" } })
+            .then(function (stream) {
+                video.srcObject = stream;
+                video.play();
+                video.style.display = 'block';
+                alert("Камера запущена. Добавим сканер в следующем этапе!");
+            })
+            .catch(function (err) {
+                console.error("Ошибка доступа к камере:", err);
+            });
+    } else {
+        alert("Ваш браузер не поддерживает доступ к камере");
+    }
+}
+
+function openCart() {
+    alert("Функция корзины в разработке");
+}
+
+function openLogin() {
+    alert("Форма входа будет доступна в следующем обновлении");
+}
